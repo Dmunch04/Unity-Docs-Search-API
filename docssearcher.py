bc003e744c1130810bf60d82ab20020f099a283c
Daniel Munch <daniellmunch@gmail.com>
2019-02-18 16:28:24 +0100

    Optimized Code

--- docssearcher.py
+++ docssearcher.py
@@ -1,15 +1,6 @@
 import sys
 import json
-if sys.version < '3':
-    from urllib2 import urlopen
-    from urllib import quote as urlquote
-else:
-    from urllib.request import urlopen
-    from urllib.parse import quote as urlquote
 
-URL_SEARCH_MANUAL = 'http://munchii.me/unitydocs/manual.json'
-URL_SEARCH_SCRIPT = 'http://munchii.me/unitydocs/script.json'
-    
 class DocsResult (object):
   def __init__ (self, title, description, url):
     self.title = title
@@ -20,18 +11,9 @@ class DocsResult (object):
     return '%s: %s (%d)' % (self.title, self.description, self.url)
 
 def get_search_json (docs):
-  if docs == "script":
-    #script = open('UnityDocs/script.json', 'r')
-    script = urlopen(URL_SEARCH_SCRIPT).read()
-    #jsonData = json.load(script)
-    jsonData = json.loads(script)
-    #script.close()
-  else:
-    #manual = open('UnityDocs/manual.json', 'r')
-    manual = urlopen(URL_SEARCH_MANUAL).read()
-    #jsonData = json.load(manual)
-    jsonData = json.loads(manual)
-    #manual.close()
+  rawData = open('UnityDocs.' + docs + '.json')
+
+  jsonData = json.load(rawData)
 
   return jsonData
 
@@ -43,6 +25,7 @@ def parse_search_json (searchItem, json):
       result = DocsResult(str(element['title']), str(element['description']), str(element['link']))
       results.append(result)
 
+  # Check for the result closest to the searchItem instead here
   return results[0]
 
 def search (search, docs):
